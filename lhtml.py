f67c6defbfcd305c2fe2d563b89a2f36ab0d454c
cyberf1y <114178507+cyberf1y@users.noreply.github.com>
2022-10-17 21:51:28 +0300

    Strip the names of the _trees

--- lhtml.py
+++ lhtml.py
@@ -19,58 +19,57 @@ def main():
         if href_is_root_relative and href not in pages:
             # /path/to/page.html is produced using path/to/page.xml
             try:
-                page_tree = ElementTree.parse(f'{href[1:-5]}.xml')
+                page = ElementTree.parse(f'{href[1:-5]}.xml')
             except ElementTree.ParseError as err:
                 raise LHTMLError(f'{href[1:-5]}.xml: {err}') from err
 
             hrefs.update(
-                a.attrib['href'] for a in page_tree.iterfind('.//a[@href]'))
-            pages[href] = page_tree
+                a.attrib['href'] for a in page.iterfind('.//a[@href]'))
+            pages[href] = page
 
-    template_tree = pages.pop('/template.html')
-    template_tree_root = template_tree.getroot()
-    title_element = template_tree.find('head/title')
-    title = title_element.text
-    template_main_tree = template_tree.find('.//main')
+    template = pages.pop('/template.html')
+    title = template.find('head/title')
+    title_text = title.text
+    template_main = template.find('.//main')
 
-    for href, page_tree in pages.items():
-        page_tree_attrib = page_tree.getroot().attrib
+    for href, page in pages.items():
+        page_attrib = page.getroot().attrib
 
         # fill the title
-        if 'title' in page_tree_attrib:
-            title_element.text = f'{page_tree_attrib["title"]} | {title}'
-            for e in page_tree.iterfind('.//*[@title-value="true"]'):
+        if 'title' in page_attrib:
+            title.text = f'{page_attrib["title"]} | {title_text}'
+            for e in page.iterfind('.//*[@title-value="true"]'):
                 del e.attrib['title-value']
-                e.text = page_tree_attrib['title']
+                e.text = page_attrib['title']
         else:
-            title_element.text = title
+            title.text = title_text
 
         # import values
-        if 'import' in page_tree_attrib and 'values' in page_tree_attrib:
+        if 'import' in page_attrib and 'values' in page_attrib:
             # load the tree only if it isn't already loaded
-            import_href = f'/{page_tree_attrib["import"][:-4]}.html'
+            import_href = f'/{page_attrib["import"][:-4]}.html'
             if import_href in pages:
-                imported_tree = pages[import_href]
+                imported = pages[import_href]
             else:
-                imported_tree = ElementTree.parse(page_tree_attrib['import'])
+                imported = ElementTree.parse(page_attrib['import'])
 
-            for key_value in page_tree_attrib['values'].split(';'):
+            for key_value in page_attrib['values'].split(';'):
                 key, value = key_value.split('=')
-                for e in page_tree.iterfind(f'.//*[@import-value="{key}"]'):
+                for e in page.iterfind(f'.//*[@import-value="{key}"]'):
                     del e.attrib['import-value']
-                    import_value_element = imported_tree.find(value)
-                    if import_value_element.text:
-                        e.text = import_value_element.text
-                    elif f'{key}-value' in import_value_element.attrib:
-                        e.text = imported_tree.getroot().attrib[key]
+                    import_value = imported.find(value)
+                    if import_value.text:
+                        e.text = import_value.text
+                    elif f'{key}-value' in import_value.attrib:
+                        e.text = imported.getroot().attrib[key]
 
         # fill the template's main
-        template_main_tree.clear()
-        template_main_tree.extend(page_tree.iterfind('.*'))
+        template_main.clear()
+        template_main.extend(page.iterfind('.*'))
         # available since python 3.9
-        # ElementTree.indent(template_tree, space='  ', level=0)
+        # ElementTree.indent(template, space='  ', level=0)
         page = ElementTree.tostring(
-            template_tree_root, encoding='utf-8', method='html')
+            template.getroot(), encoding='utf-8', method='html')
 
         # write the page
         with open(href[1:], 'wb') as file:
