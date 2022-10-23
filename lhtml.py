d6108a6ac301d68e7c46db42859e81d5957973dd
cyberf1y <114178507+cyberf1y@users.noreply.github.com>
2022-10-23 13:23:06 +0300

    Check if a file appears more than once in the args

--- lhtml.py
+++ lhtml.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
+from collections import Counter
+
+
+class LHTMLError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
 
 
 def main():
     parser = ArgumentParser(description='link HTML files')
     parser.add_argument(dest='files', metavar='FILE', nargs='+')
     parser.add_argument('-o', '--output', metavar='DIR', default='output')
-    parser.parse_args()
+    args = parser.parse_args()
+
+    for file, n in Counter(args.files).items():
+        if n > 1:
+            raise LHTMLError(f'"{file}" appears {n} times in the argument list')
 
 
 if __name__ == '__main__':
