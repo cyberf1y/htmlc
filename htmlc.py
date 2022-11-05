caedd19fff062744daea2f01dea101ba36838f8a
cyberf1y <114178507+cyberf1y@users.noreply.github.com>
2022-11-05 12:55:14 +0200

    Improve the errors

--- htmlc.py
+++ htmlc.py
@@ -6,10 +6,24 @@ import sys
 from xml.etree import ElementTree
 
 
-class HTMLCError(Exception):
+class HTMLCArgumentError(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
+
+class HTMLCImportError(Exception):
+    def __init__(self, file, message):
+        self.file = file
+        self.message = message
+        super().__init__(f'{self.file}: {self.message}')
+
+
+class HTMLCParseError(Exception):
+    def __init__(self, file, message):
+        self.message = message
+        self.file = file
+        super().__init__(f'{self.file}: {self.message}')
 
 
 def main():
@@ -20,22 +34,35 @@ def main():
 
     for file, n in Counter(args.files).items():
         if n > 1:
-            raise HTMLCError(f'"{file}" appears {n} times in the argument list')
+            raise HTMLCArgumentError(f'"{file}" passed more ({n}) than once')
 
     loaded_file_trees = dict()
-    def get_loaded_file_tree(file):
+    def load_file_tree(file):
         try:
             return loaded_file_trees.setdefault(file, ElementTree.parse(file))
-        except ElementTree.ParseError as err:
-            raise HTMLCError(f'{file}: {err}') from err
+        except ElementTree.ParseError as e:
+            raise HTMLCParseError(file, e) from e
 
     for file in args.files:
         file_dir = os.path.dirname(file)
-        file_tree = get_loaded_file_tree(file)
+        try:
+            file_tree = load_file_tree(file)
+        except FileNotFoundError as e:
+            raise HTMLCArgumentError(f'"{file}" not found') from e
+
         for importee in file_tree.iterfind('.//*[@import]'):
-            import_file, xpath = importee.attrib['import'].split(':')
+            try:
+                import_file, xpath = importee.attrib['import'].split(':')
+            except ValueError as e:
+                bad_syntax = f'bad syntax: "import={importee.attrib["import"]}"'
+                raise HTMLCImportError(file, bad_syntax) from e
+
             import_path = os.path.join(file_dir, import_file)
-            imported = get_loaded_file_tree(import_path).find(xpath)
+            try:
+                imported = load_file_tree(import_path).find(xpath)
+            except FileNotFoundError as e:
+                import_not_found = f'"{import_path}" not found'
+                raise HTMLCImportError(file, import_not_found) from e
 
             del importee.attrib['import']
             importee.attrib.update(imported.attrib)
